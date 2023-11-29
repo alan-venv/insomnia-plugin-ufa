aa4c3e985124b0315f38e5bee98423e66ebb8c00
Alan Silva <tmp.venv@proton.me>
2023-11-29 15:59:10 -0300

    :zap:

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ module.exports.templateTags = [
         name: 'geradorDeCPF',
         displayName: 'CPF',
         description: 'Gera um CPF válido aleatório',
-        async run() {
+        args: [
+            {
+                displayName: 'Com mascara?',
+                description: 'Adicionar mascara',
+                type: 'boolean',
+                defaultValue: false
+            }
+        ],
+        async run(context, mask) {
             const rnd = (n) => Math.round(Math.random() * n);
             const mod = (base, div) => Math.round(base - Math.floor(base / div) * div)
             const n = Array(9).fill('').map(() => rnd(9));
@@ -13,7 +21,9 @@ module.exports.templateTags = [
             let d2 = (d1 * 2) + n.reduce((total, number, index) => (total + (number * (11 - index))), 0)
             d2 = 11 - mod(d2, 11);
             if (d2 >= 10) d2 = 0;
-            return `${n.join('')}${d1}${d2}`
+            return (mask) ?
+                `${n.slice(0, 3).join('')}.${n.slice(3, 6).join('')}.${n.slice(6, 9).join('')}-${d1}${d2}` :
+                `${n.join('')}${d1}${d2}`
         }
     },
     {
